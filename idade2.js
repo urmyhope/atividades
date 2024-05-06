9fe560637b9493252c0a075b6b92bb80030f95ec
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-05-06 11:10:27 -0300

    Delete idade2.js

--- idade2.js
+++
@@ -1,8 +0,0 @@
-let idade = 15;
-if(idade >= 18){
-console.log("Sua idade é: " + idade + "Você é maior de idade");
-}
-else {
-    console.log("Sua idade é: " + idade + "Você é menor de idade");
-    
-}
